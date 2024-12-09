40a77564780840461a45b1bfed8447e0aaa60202
Rawnak Hasan <rh.rawnakhasan@gmail.com>
2024-12-09 14:49:12 +0600

    gmail

--- mail.php
+++ mail.php
@@ -1,14 +1,14 @@
 <?php
 $firstn = $_POST['RAWNAK'];
 $lastn = $_POST['HASAN'];
-$email = $_POST['rh.rawnakhasan@gmail.com'];
+$email = $_POST['rawnak.h625@gmail.com'];
 $phone = $_POST['+8801331336920'];
 $services = $_POST['services'];
 $message = $_POST['message'];
-$formcontent="First Name: RAWNAK \n Last Name: RAWNAK \n Email: rh.rawnakhasan@gmail.com \n Services: services \n Phone: $+8801331336920 \n Message: $message";
-$recipient = "rh.rawnakhasan@gmail.com";
+$formcontent="First Name: RAWNAK \n Last Name: RAWNAK \n Email: rawnak.h625@gmail.com \n Services: services \n Phone: $+8801331336920 \n Message: $message";
+$recipient = "rawnak.h625@gmail.com";
 $subject = "Contact Form";
-$mailheader = "From: rh.rawnakhasan@gmail.com \r\n";
+$mailheader = "From: rawnak.h625@gmail.com \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 require_once "thank-you.html";
 ?>
--- news-mail.php
+++ news-mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 
 $formcontent="Email: $email";
-$recipient = "rh.rawnakhasan@gmail.com";
+$recipient = "rawnak.h625@gmail.com";
 $subject = "Newslater Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
