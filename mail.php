b7316901a3ca5b99085813a5b5b9ee92a081c867
Rawnak Hasan <rh.rawnakhasan@gmail.com>
2024-12-09 14:50:42 +0600

    ph

--- mail.php
+++ mail.php
@@ -2,10 +2,10 @@
 $firstn = $_POST['RAWNAK'];
 $lastn = $_POST['HASAN'];
 $email = $_POST['rawnak.h625@gmail.com'];
-$phone = $_POST['+8801331336920'];
+$phone = $_POST['+8801310502625'];
 $services = $_POST['services'];
 $message = $_POST['message'];
-$formcontent="First Name: RAWNAK \n Last Name: RAWNAK \n Email: rawnak.h625@gmail.com \n Services: services \n Phone: $+8801331336920 \n Message: $message";
+$formcontent="First Name: RAWNAK \n Last Name: RAWNAK \n Email: rawnak.h625@gmail.com \n Services: services \n Phone: $+8801310502625 \n Message: $message";
 $recipient = "rawnak.h625@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: rawnak.h625@gmail.com \r\n";
